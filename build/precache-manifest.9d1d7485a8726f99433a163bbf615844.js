668ab7a6c71a643bb25299b16584f0f8d9390184
Александр <cenitelas@mail.ru>
2019-11-17 01:28:57 +0600

    add git page

---
+++ build/precache-manifest.9d1d7485a8726f99433a163bbf615844.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d4fafe162c41982021712d750bb694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c77840e89078ac9017b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a9ac76115fc3f70129a6",
+    "url": "/static/js/2.be432a6d.chunk.js"
+  },
+  {
+    "revision": "1c77840e89078ac9017b",
+    "url": "/static/js/main.66c601f0.chunk.js"
+  },
+  {
+    "revision": "06342275598567d615bf",
+    "url": "/static/js/runtime-main.c9607c74.js"
+  }
+]);
